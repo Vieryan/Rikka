274aa29b35a449e36d2803c7cfc02516ce2873ac
Vieryan <44139752+Vieryan@users.noreply.github.com>
2018-10-15 20:42:52 +0700

    Update rikka.js

--- rikka.js
+++ rikka.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const TOKEN ='NTAwMjc2NzI5MDMwNjM5NjI3.DqTbAw.6i41Lrz3Yy7uLR58X3lyXUEy4uo';
+const TOKEN ='NDg1Mjg5MjU2NjcyMTY1ODg5.DqYqkQ.uI_LIyKoZmHQUsdh_KeWVL3LOzo';
 
 var bot = new Discord.Client();
 
